f731df5b2bdd7768974347b8fe021c7712b41224
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-04-22 00:56:02 +0300

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
 
-const galleryContainer = document.querySelector('.js-gallery');
+const galleryContainer = document.querySelector('.gallery');
 
 const cardsMarkup = createGalleryItemCardMarkup(galleryItems);
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryItemCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery li a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+function createGalleryItemCardMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+            </a>
+        </li>
+        `;
+    })
+    .join('');
+};
+
+
